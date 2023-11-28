e4ea1a1791d532d31bea76412dab0ee63ba6fdfc
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-28 17:34:45 -0300

    adding AppError class for exception handling

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
 
@@ -6,5 +9,21 @@ const PORT = 3333;
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.log(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "intermal server error"
+  });
+});
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
