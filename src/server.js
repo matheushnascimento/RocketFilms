850327e39cf3272cf1972eeebe9aea86d7449889
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-28 17:14:22 -0300

    adding users.router.js and routes/index.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const routes = require("./routes");
+
 const app = express();
-const PORT = 3333
+const PORT = 3333;
 
-app.post("/", (request, response) => {
-  const {name, email, password} = request.body;
-
-  response.json({name, email, password})
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
