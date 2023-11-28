a09af1722c7c772b15975740cdde89148c6f8f97
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-28 14:21:09 -0300

    adding dev mod nodemon

--- src/server.js
+++ src/server.js
@@ -2,4 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3333
 
+app.post("/", (request, response) => {
+  const {name, email, password} = request.body;
+
+  response.json({name, email, password})
+})
+
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
